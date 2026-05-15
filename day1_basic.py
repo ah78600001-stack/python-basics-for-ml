cf685557f01506e729647c6f17c47e1d0915f136
ah78600001-stack <ah78600001.com>
2026-05-15 14:48:01 +0500

    Added day 1 Python basics

---
+++ day1_basic.py
@@ -0,0 +1,6 @@
+name = "Ali Hussain"
+university ="superior university lahore"
+course ="information technology"
+print("my name is " + name)
+print ("i am a student of " + university)
+print("i am currently enrolled in " + course)
